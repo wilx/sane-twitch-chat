f49c9c02505113e0611dfdb6ddcf4d862c98f294
Vaclav Haisman <vhaisman@gmail.com>
2024-09-19 23:08:01 +0200

    Bump up Firefox and Chrome versions.

--- output/index.user.js
+++ output/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        sane-twitch-chat
 // @description Twitch chat sanitizer.
-// @version     1.0.500
+// @version     1.0.502
 // @author      wilx
 // @homepage    https://github.com/wilx/sane-twitch-chat
 // @supportURL  https://github.com/wilx/sane-twitch-chat/issues
