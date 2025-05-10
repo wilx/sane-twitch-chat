fc1995ebdd7e7603867e0deaf80704fdecd1de4e
Vaclav Haisman <vhaisman@gmail.com>
2025-05-10 16:38:15 +0200

    Updates.

--- output/index.user.js
+++ output/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        sane-twitch-chat
 // @description Twitch chat sanitizer.
-// @version     1.0.549
+// @version     1.0.550
 // @author      wilx
 // @homepage    https://github.com/wilx/sane-twitch-chat
 // @supportURL  https://github.com/wilx/sane-twitch-chat/issues
