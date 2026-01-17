afe374c83c16c05d0cbfd5d241c8702c99f17347
Vaclav Haisman <vhaisman@gmail.com>
2026-01-17 20:46:55 +0100

    Update to Yarn 4.12.0.

--- output/index.user.js
+++ output/index.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        sane-twitch-chat
 // @description Twitch chat sanitizer.
-// @version     1.0.562
+// @version     1.0.563
 // @author      wilx
 // @homepage    https://github.com/wilx/sane-twitch-chat
 // @supportURL  https://github.com/wilx/sane-twitch-chat/issues
